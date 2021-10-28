bd9c7a5f8bd24e239b981c8d04875633d1485f19
adco1997 <adco08191997@gmail.com>
2021-10-27 20:28:29 -0600

    actuentasalizacion form de v

--- HNSolutionS_TEST/Ventas.Designer.vb
+++ HNSolutionS_TEST/Ventas.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Ventas
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,178 @@ Partial Class Ventas
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Buscar = New System.Windows.Forms.Button()
+        Me.Editar = New System.Windows.Forms.Button()
+        Me.agregar = New System.Windows.Forms.Button()
+        Me.Eliminar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.factura = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Cantidad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cocliente = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.telefono = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Direccion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(62, 98)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(223, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Buscar
+        '
+        Me.Buscar.Location = New System.Drawing.Point(291, 98)
+        Me.Buscar.Name = "Buscar"
+        Me.Buscar.Size = New System.Drawing.Size(75, 23)
+        Me.Buscar.TabIndex = 1
+        Me.Buscar.Text = "Buscar"
+        Me.Buscar.UseVisualStyleBackColor = True
+        '
+        'Editar
+        '
+        Me.Editar.Location = New System.Drawing.Point(400, 98)
+        Me.Editar.Name = "Editar"
+        Me.Editar.Size = New System.Drawing.Size(75, 23)
+        Me.Editar.TabIndex = 2
+        Me.Editar.Text = "Editar"
+        Me.Editar.UseVisualStyleBackColor = True
+        '
+        'agregar
+        '
+        Me.agregar.Location = New System.Drawing.Point(481, 98)
+        Me.agregar.Name = "agregar"
+        Me.agregar.Size = New System.Drawing.Size(75, 23)
+        Me.agregar.TabIndex = 3
+        Me.agregar.Text = "Añadir V."
+        Me.agregar.UseVisualStyleBackColor = True
+        '
+        'Eliminar
+        '
+        Me.Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Eliminar.Location = New System.Drawing.Point(562, 98)
+        Me.Eliminar.Name = "Eliminar"
+        Me.Eliminar.Size = New System.Drawing.Size(75, 23)
+        Me.Eliminar.TabIndex = 4
+        Me.Eliminar.Text = "Eliminar"
+        Me.Eliminar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, -1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(712, 38)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "VENTAS"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(265, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(187, 18)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "IMPORTADORA NOELIA"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Location = New System.Drawing.Point(53, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(319, 42)
+        Me.Label3.TabIndex = 12
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.factura, Me.Cantidad, Me.cocliente, Me.telefono, Me.Direccion, Me.descripcion})
+        Me.DataGridView1.Location = New System.Drawing.Point(38, 163)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(643, 150)
+        Me.DataGridView1.TabIndex = 13
+        '
+        'factura
+        '
+        Me.factura.HeaderText = "No. Factura"
+        Me.factura.Name = "factura"
+        '
+        'Cantidad
+        '
+        Me.Cantidad.HeaderText = "Cantidad"
+        Me.Cantidad.Name = "Cantidad"
+        '
+        'cocliente
+        '
+        Me.cocliente.HeaderText = "Co. Cliente"
+        Me.cocliente.Name = "cocliente"
+        '
+        'telefono
+        '
+        Me.telefono.HeaderText = "No. Telefono"
+        Me.telefono.Name = "telefono"
+        '
+        'Direccion
+        '
+        Me.Direccion.HeaderText = "Direccion"
+        Me.Direccion.Name = "Direccion"
+        '
+        'descripcion
+        '
+        Me.descripcion.HeaderText = "Descripcion"
+        Me.descripcion.Name = "descripcion"
+        '
+        'Ventas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(711, 384)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Eliminar)
+        Me.Controls.Add(Me.agregar)
+        Me.Controls.Add(Me.Editar)
+        Me.Controls.Add(Me.Buscar)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Ventas"
         Me.Text = "Ventas"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Buscar As Button
+    Friend WithEvents Editar As Button
+    Friend WithEvents agregar As Button
+    Friend WithEvents Eliminar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents factura As DataGridViewTextBoxColumn
+    Friend WithEvents Cantidad As DataGridViewTextBoxColumn
+    Friend WithEvents cocliente As DataGridViewTextBoxColumn
+    Friend WithEvents telefono As DataGridViewTextBoxColumn
+    Friend WithEvents Direccion As DataGridViewTextBoxColumn
+    Friend WithEvents descripcion As DataGridViewTextBoxColumn
 End Class
