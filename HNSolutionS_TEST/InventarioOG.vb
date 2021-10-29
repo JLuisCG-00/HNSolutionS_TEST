80b81a8f748131a2b2e523f415e86d7577bd8bba
Andrea Ramos <andreamramos_97@hotmail.com>
2021-10-29 15:23:20 -0600

    El diseño del formulario de inventario se completo.

--- HNSolutionS_TEST/InventarioOG.vb
+++ HNSolutionS_TEST/InventarioOG.vb
@@ -1,3 +1,9 @@
 ﻿Public Class InventarioOG
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CmbSucursal.SelectedIndexChanged
 
+    End Sub
+
+    Private Sub InventarioOG_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
