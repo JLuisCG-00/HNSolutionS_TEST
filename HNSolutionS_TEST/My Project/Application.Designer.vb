afebee542e3bc8739e8af442f3f1399a7eea6cce
Jose L Calix G <jlcalixgaldamez@gmail.com>
2021-11-24 10:55:49 -0600

    Se agrego la opcion de ingresar arículos

--- HNSolutionS_TEST/My Project/Application.Designer.vb
+++ HNSolutionS_TEST/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HNSolutionS_TEST.Form1
+            Me.MainForm = Global.HNSolutionS_TEST.LOGIN
         End Sub
     End Class
 End Namespace
