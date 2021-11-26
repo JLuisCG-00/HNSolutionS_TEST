4628fef257d5ed692ed0b5fb34ba2bc2db1352b6
Andrea Ramos <andreamramos_97@hotmail.com>
2021-11-26 11:50:11 -0600

    CAMBIO OFICIAL - ESTE ES EL BUENO

--- HNSolutionS_TEST/ventas.vb
+++ HNSolutionS_TEST/ventas.vb
@@ -2,10 +2,9 @@
 Public Class ventas
     Private Sub ventas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim dt As DataTable = New DataTable
-        Dim dt2 As DataTable = New DataTable
         Using Sql As New SqlConnection("Data Source=localhost;Initial Catalog=HNSolutionS2;Integrated Security=True")
             Sql.Open()
-            Dim da As New SqlDataAdapter("exec ListaArticulosFact", Sql)
+            Dim da As New SqlDataAdapter("exec LlamarInventario2", Sql)
             dt = New DataTable
             da.Fill(dt)
             DGClientes.DataSource = dt
