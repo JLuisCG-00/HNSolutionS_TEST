ded427ab6230f13324f52e8743e12b66fdfa8b75
gamezjavier12eladio <gamezjavier12eladio@gmail.com>
2021-12-03 19:18:42 -0600

    diseño

--- HNSolutionS_TEST/FormularioGVD.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class FormularioGVD
-
-End Class
--- HNSolutionS_TEST/My Project/Resources.Designer.vb
+++ HNSolutionS_TEST/My Project/Resources.Designer.vb
@@ -79,5 +79,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property icons8_búsqueda_50() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("icons8-búsqueda-50", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
